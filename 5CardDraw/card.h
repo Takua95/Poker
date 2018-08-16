d9da31a4d09575a04c8a69a321fa6ff6d704ed00
Christian Brannon <cabrannon54955@mymail.tstc.edu>
2018-08-16 05:57:13 -0500

    Add files via upload

--- 5CardDraw/card.h
+++ 5CardDraw/card.h
@@ -1,13 +1,15 @@
 #pragma once
-#include <iostream>
-#include <vector>
-#include <string>
+#include "stdafx.h"
 using namespace std;
-
 
 class Card
 {
+public:
+	//ENUMERATED DATA TYPES
 	enum Face { three, four, five, six, seven, eight, nine, ten, jack, queen, king, joker };
 	enum Suit { diamonds, spades, hearts, stars, clubs, none };
+protected:
+
+private:
 
 };

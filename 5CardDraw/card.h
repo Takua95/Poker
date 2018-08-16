41812dc7f3d53e4d17ef304c411b697500763f1a
Christian Brannon <cabrannon54955@mymail.tstc.edu>
2018-08-16 07:12:45 -0500

    Add files via upload

--- 5CardDraw/card.h
+++ 5CardDraw/card.h
@@ -6,8 +6,8 @@ class Card
 {
 public:
 	//ENUMERATED DATA TYPES
-	enum Face { ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king };
-	enum Suit { spades, hearts, diamonds, clubs };
+	enum Face { ace = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, ten = 10, jack = 11, queen = 12, king = 13 };
+	enum Suit { spades = 0, hearts = 13, diamonds = 26, clubs = 39 };
 protected:
 
 private:

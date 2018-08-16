2c7325b2171eef09f2eb138aa4063c5e3310f614
Christian Brannon <cabrannon54955@mymail.tstc.edu>
2018-08-16 05:17:57 -0500

    Add files via upload

---
+++ 5CardDraw/card.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Card
+{
+	enum Face { three, four, five, six, seven, eight, nine, ten, jack, queen, king, joker };
+	enum Suit { diamonds, spades, hearts, stars, clubs, none };
+
+};
